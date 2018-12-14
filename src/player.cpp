52014f9d068087be724d7395eb7a5a254a2e7665
Hunter Lannon <hunter.d.lannon@gmail.com>
2018-12-13 20:29:55 -0800

    stick now shows up

--- src/player.cpp
+++ src/player.cpp
@@ -1,9 +1,8 @@
 #include "../inc/player.h"
 
 Player::Player() {
-	sf::Texture stick_tex;
-	stick_tex.loadFromFile("img/sticky.png");
-	hitbox = sf::Sprite(static_stick, sf::Rect<int>(400,400,50,100));
+	static_stick.loadFromFile("img/sticky.png");
+	hitbox.setTexture(static_stick);
 }
 
 void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
@@ -19,10 +18,10 @@ void Player::action(int Key) {
 			hitbox.move(sf::Vector2f(5,0));
 			break;
 		case sf::Keyboard::W:
-			hitbox.move(sf::Vector2f(0,5));
+			hitbox.move(sf::Vector2f(0,-5));
 			break;
 		case sf::Keyboard::S:
-			hitbox.move(sf::Vector2f(0,-5));
+			hitbox.move(sf::Vector2f(0,5));
 			break;
 	}
 }
